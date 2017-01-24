ed2b2b64aba1b547f75ba8350aa5eff96232c124
Tim Schön <timschoen95@googlemail.com>
2017-01-24 17:17:07 +0100

    Added missing resources

---
+++ strategygame-client/BuildingWindow.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using strategygame_common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strategygame_client
+{
+    class BuildingWindow : Window
+    {
+        int SelectedSlot;
+        Texture2D BuildingSlotTexture;
+        IBuildingInformation BuildingInformation;
+
+        public BuildingWindow(string Name, ContentManager Content, int X, int Y, IBuildingInformation BuildingInformation) : base(Name, Content, X, Y)
+        {
+            WindowRectangle.Width = 200;
+            WindowRectangle.Height = 300;
+            isOpen = false;
+            BuildingSlotTexture = Content.Load<Texture2D>("UI/Windows/BuildingSlot");
+            this.BuildingInformation = BuildingInformation;
+        }
+
+        public void setSlot(int SelectedSlot)
+        {
+            this.SelectedSlot = SelectedSlot;
+            isOpen = true;
+        }
+
+        public void Draw(IVillage Village, SpriteBatch spriteBatch, float Layer)
+        {
+            base.Draw(spriteBatch, Layer);
+
+            if (Village == null)
+                isOpen = false;
+
+            if (Village.Buildings.Length <= SelectedSlot)
+                isOpen = false;
+
+            if (!isOpen)
+                return;
+
+            Point BuildingAndLevel = Village.Buildings[SelectedSlot];
+
+            if(BuildingAndLevel == null)
+            {
+                //empty Building Slot
+                Name = "Leerer Gebäude-Slot";
+                WindowRectangle.Height = 500;
+            }
+            else
+            {
+                //show Building Information
+                Name = BuildingInformation.getBuildingInfo(BuildingAndLevel.X).Name;
+                WindowRectangle.Height = 300;
+
+                List<int> BuildableBuildings = BuildingInformation.BuildableBuildings(Village.Buildings, Village.CellType);
+
+            }
+        }
+
+        private void RenderBuildingOption(SpriteBatch spriteBatch, float Layer, int BuildingType, IVillage Village)
+        {
+
+        }
+    }
+}
---
+++ strategygame-client/MouseHandler.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strategygame_client
+{
+    class MouseHandler
+    {
+        public delegate void MouseClick(Point p);
+        public delegate void Selection(Point A, Point B);
+
+        public event MouseClick OnMouseClick;
+        public event Selection OnSelection;
+
+        private Texture2D CursorTexture;
+        private Texture2D SelectedBGTexture;
+
+        TimeSpan DragClickThreshhold = TimeSpan.FromSeconds(1);
+
+        DateTime MousePressStarted;
+        bool MouseWasPressed = false;
+        Point MousePressStartPosition;
+        
+        bool isDragging;
+
+        public MouseHandler(MouseClick OnMouseClick, Selection OnSelection, ContentManager Content)
+        {
+            this.OnMouseClick = OnMouseClick;
+            this.OnSelection = OnSelection;
+            this.CursorTexture = Content.Load<Texture2D>("UI/Mouse/Cursor");
+            this.SelectedBGTexture = Content.Load<Texture2D>("UI/Mouse/SelectedBG");
+        }
+
+        public void Update()
+        {
+            MouseState MS = Mouse.GetState();
+            if(MS.LeftButton == ButtonState.Pressed)
+            {
+                if(!MouseWasPressed)
+                {
+                    MouseWasPressed = true;
+                    MousePressStarted = DateTime.Now;
+                    MousePressStartPosition = MS.Position;
+                }
+                else
+                {
+                    if(DateTime.Now - MousePressStarted > DragClickThreshhold)
+                    {
+                        isDragging = true;
+                    }
+                }
+            }
+            else
+            {
+                if (MouseWasPressed)
+                {
+                    if(DateTime.Now - MousePressStarted > DragClickThreshhold)
+                    {
+                        //Dragging finished
+                        if (OnSelection != null)
+                            OnSelection(MousePressStartPosition, MS.Position);
+
+                        isDragging = false;
+                    }
+                    else
+                    {
+                        //normal mouse click occured
+                        if (OnMouseClick != null)
+                            OnMouseClick(MS.Position);
+                    }
+
+                    MouseWasPressed = false;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, float LayerDepth)
+        {
+            spriteBatch.Draw(texture: CursorTexture, position: new Vector2(Mouse.GetState().X, Mouse.GetState().Y), layerDepth: LayerDepth);
+
+            if(isDragging)
+            {
+                Point A = new Point(Math.Min(MousePressStartPosition.X, Mouse.GetState().Position.X), Math.Min(MousePressStartPosition.Y, Mouse.GetState().Position.Y));
+                Point RectangleSize = new Point(Math.Max(MousePressStartPosition.X, Mouse.GetState().Position.X)-A.X, Math.Max(MousePressStartPosition.Y, Mouse.GetState().Position.Y)-A.Y);
+
+                spriteBatch.Draw(texture: SelectedBGTexture, destinationRectangle: new Rectangle(A, RectangleSize), layerDepth: LayerDepth-0.001f);
+            }
+        }
+    }
+}
