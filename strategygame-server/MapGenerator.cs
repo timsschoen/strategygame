4c2ff10ca592b11eacbf3ffce88e825b652f889f
DrSmiddy <felix_schmidtmann@web.de>
2017-04-03 17:49:55 +0200

    added MapGenerator

---
+++ strategygame-server/MapGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using strategygame_common;
+
+namespace strategygame_server
+{
+	public class MapGenerator
+	{
+		Map map { get;}
+
+		public MapGenerator (int x, int y)
+		{
+			map = new Map (x, y);
+		}
+
+		public void generateMap ()
+		{
+			for (int i = 0; i < map.Width; i++) {
+				for (int j = 0; j < map.Height; j++) {
+					Random random = new Random ();
+					Resources res = new Resources ();
+					for (int k = 0; k < 5; k++) {
+						res.SetResourceCount (k, random.Next (1000));
+					}
+					MapCell newCell = new MapCell (random.Next (10), res);
+					map.addCell (i, j, newCell);
+				}
+			}
+		}
+	}
+}
+
